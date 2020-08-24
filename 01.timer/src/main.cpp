2b761945a0ce2bcb4764fb4e954208bee5ffe1a4
Đặng Quang Hưng <hungdq58@gmail.com>
2020-08-24 18:41:54 +0700

    Synchronous timer

---
+++ 01.timer/src/main.cpp
@@ -0,0 +1,12 @@
+#include <chrono>
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main()
+{
+    boost::asio::io_context io;
+    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+    t.wait();
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
